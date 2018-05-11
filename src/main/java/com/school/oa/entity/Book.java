590f15b47ee1ec7836ce87e6b272719f288b0e6f
ZHENGZX123 <656701179@qq.com>
2018-05-11 11:04:53 +0800

    Android 接口调试

--- src/main/java/com/school/oa/entity/Book.java
+++ src/main/java/com/school/oa/entity/Book.java
@@ -6,31 +6,84 @@ import java.sql.Timestamp;
 public class Book implements Serializable{
 	
 	private Integer id;
-	private String theme;
-	private String content;
+	private String book_name;
+	private String book_code;
+	private Integer borrow_id;
+	private String borrow_name;
+	private Timestamp borrow_time;
+	private Integer borrow_num;
+	private Integer give_id;
+	private String give_name;
+	private Timestamp give_time;
+	private Integer state;
 	private Timestamp create_time;
 	private Timestamp update_time;
-	private String is_active;
-    private Integer user_id;
-    private String user_name;
-    
+	private Integer is_active;
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getTheme() {
-		return theme;
+	public String getBook_name() {
+		return book_name;
 	}
-	public void setTheme(String theme) {
-		this.theme = theme;
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
 	}
-	public String getContent() {
-		return content;
+	public String getBook_code() {
+		return book_code;
 	}
-	public void setContent(String content) {
-		this.content = content;
+	public void setBook_code(String book_code) {
+		this.book_code = book_code;
+	}
+	public Integer getBorrow_id() {
+		return borrow_id;
+	}
+	public void setBorrow_id(Integer borrow_id) {
+		this.borrow_id = borrow_id;
+	}
+	public String getBorrow_name() {
+		return borrow_name;
+	}
+	public void setBorrow_name(String borrow_name) {
+		this.borrow_name = borrow_name;
+	}
+	public Timestamp getBorrow_time() {
+		return borrow_time;
+	}
+	public void setBorrow_time(Timestamp borrow_time) {
+		this.borrow_time = borrow_time;
+	}
+	public Integer getBorrow_num() {
+		return borrow_num;
+	}
+	public void setBorrow_num(Integer borrow_num) {
+		this.borrow_num = borrow_num;
+	}
+	public Integer getGive_id() {
+		return give_id;
+	}
+	public void setGive_id(Integer give_id) {
+		this.give_id = give_id;
+	}
+	public String getGive_name() {
+		return give_name;
+	}
+	public void setGive_name(String give_name) {
+		this.give_name = give_name;
+	}
+	public Timestamp getGive_time() {
+		return give_time;
+	}
+	public void setGive_time(Timestamp give_time) {
+		this.give_time = give_time;
+	}
+	public Integer getState() {
+		return state;
+	}
+	public void setState(Integer state) {
+		this.state = state;
 	}
 	public Timestamp getCreate_time() {
 		return create_time;
@@ -44,31 +97,20 @@ public class Book implements Serializable{
 	public void setUpdate_time(Timestamp update_time) {
 		this.update_time = update_time;
 	}
-	public String getIs_active() {
+	public Integer getIs_active() {
 		return is_active;
 	}
-	public void setIs_active(String is_active) {
+	public void setIs_active(Integer is_active) {
 		this.is_active = is_active;
 	}
-	public Integer getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(Integer user_id) {
-		this.user_id = user_id;
-	}
-	public String getUser_name() {
-		return user_name;
-	}
-	public void setUser_name(String user_name) {
-		this.user_name = user_name;
-	}
-	
 	@Override
 	public String toString() {
-		return "Book [id=" + id + ", theme=" + theme + ", content=" + content + ", create_time=" + create_time
-				+ ", update_time=" + update_time + ", is_active=" + is_active + ", user_id=" + user_id + ", user_name="
-				+ user_name + "]";
+		return "Book [id=" + id + ", book_name=" + book_name + ", book_code=" + book_code + ", borrow_id=" + borrow_id
+				+ ", borrow_name=" + borrow_name + ", borrow_time=" + borrow_time + ", borrow_num=" + borrow_num
+				+ ", give_id=" + give_id + ", give_name=" + give_name + ", give_time=" + give_time + ", state=" + state
+				+ ", create_time=" + create_time + ", update_time=" + update_time + ", is_active=" + is_active + "]";
 	}
-    
+	
+
     
 }
